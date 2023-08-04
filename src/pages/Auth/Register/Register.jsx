90a35912038a7820613a7fc648f6f529dc6c9e24
Taras21071988 <tsasojlov@icloud.com>
2023-08-04 16:15:55 +0300

    implementation of the registration logic, and password matching checks

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,36 +1,103 @@
-import React from 'react'
-import { TextField, Button, Typography } from '@mui/material'
-import cl from './Register.module.scss'
+import React from "react";
+import { TextField, Button, Typography } from "@mui/material";
+import cl from "./Register.module.scss";
 
-const Register = () => {
+const Register = (props) => {
+  const {
+    setEmail,
+    setPassword,
+    setRepeatPassword,
+    setFirstName,
+    setUsername,
+  } = props;
+
   return (
     <div className={cl.register}>
-      <div className='container'>
-
+      <div className="container">
         <div className={cl.form}>
-          <Typography variant="h2" marginBottom={3} textAlign='center' fontFamily='Montserrat' >
+          <Typography
+            variant="h2"
+            marginBottom={3}
+            textAlign="center"
+            fontFamily="Montserrat"
+          >
             Регистрация
           </Typography>
-          <Typography variant="body1" marginBottom={3} textAlign='center' fontFamily='Montserrat' >
+          <Typography
+            variant="body1"
+            marginBottom={3}
+            textAlign="center"
+            fontFamily="Montserrat"
+          >
             Введите данные для регистрации
           </Typography>
-          <TextField fullWidth={true} margin='normal' id="outlined-basic" label="Name" variant="outlined" placeholder='Введите ваш Имя' />
-          <TextField fullWidth={true} margin='normal' id="outlined-basic" label="Username" variant="outlined" placeholder='Введите ваш Username' />
-          <TextField fullWidth={true} margin='normal' id="outlined-basic" label="Email" variant="outlined" placeholder='Введите ваш Email' />
-          <TextField type='password' fullWidth={true} margin='normal' id="outlined-basic" label="Password" variant="outlined" placeholder='Введите ваш password' />
-          <TextField type='password' fullWidth={true} margin='normal' id="outlined-basic" label="Repeat Password" variant="outlined" placeholder='Повторите ваш password' />
-          <Button sx={{ fontFamily: 'Montserrat', width: '60%', marginBottom: 2, marginTop: 2 }} variant="contained" >Зарегестрироваться</Button>
-          <Typography variant="body1" sx={{ fontFamily: 'Montserrat' }}  >У вас есть аккаунт?
+          <TextField
+            fullWidth={true}
+            margin="normal"
+            id="outlined-basic_firstName"
+            label="Name"
+            variant="outlined"
+            placeholder="Введите ваш Имя"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <TextField
+            fullWidth={true}
+            margin="normal"
+            id="outlined-basic_username"
+            label="Username"
+            variant="outlined"
+            placeholder="Введите ваш Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            fullWidth={true}
+            margin="normal"
+            id="outlined-basic_email"
+            label="Email"
+            variant="outlined"
+            placeholder="Введите ваш Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            type="password"
+            fullWidth={true}
+            margin="normal"
+            id="outlined-basic_password"
+            label="Password"
+            variant="outlined"
+            placeholder="Введите ваш password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            type="password"
+            fullWidth={true}
+            margin="normal"
+            id="outlined-basic_repeatPassword"
+            label="Repeat Password"
+            variant="outlined"
+            placeholder="Повторите ваш password"
+            onChange={(e) => setRepeatPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            sx={{
+              fontFamily: "Montserrat",
+              width: "60%",
+              marginBottom: 2,
+              marginTop: 2,
+            }}
+            variant="contained"
+          >
+            Зарегестрироваться
+          </Button>
+          <Typography variant="body1" sx={{ fontFamily: "Montserrat" }}>
+            У вас есть аккаунт?
             <span className={cl.span__register}>Войти</span>
           </Typography>
         </div>
-
-
-
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
--- src/pages/Auth/RouteAuth.jsx
+++ src/pages/Auth/RouteAuth.jsx
@@ -1,51 +1,76 @@
-import React, { useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import Login from './Login/Login'
-import Register from './Register/Register'
-import cl from './RouteAuth.module.scss'
-import { Box } from '@mui/material'
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+import cl from "./RouteAuth.module.scss";
+import { Box } from "@mui/material";
 
-import { instance } from '../../utils/axios'
-
+import { instance } from "../../utils/axios";
 
 const RouteAuth = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [username, setUsername] = useState("");
   const location = useLocation();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    const userData = {
-      email,
-      password
+    e.preventDefault();
+    if (location.pathname === "/Login") {
+      const userData = {
+        email,
+        password,
+      };
+      const user = await instance.post("/auth/login", userData);
+      console.log(user.data);
+    } else {
+      if(password===repeatPassword){
+        const userData = {
+          firstName,
+          username,
+          email,
+          password,
+        };
+        const newUser = await instance.post("/auth/register", userData);
+        console.log(newUser.data);
+      }else{
+         alert("У Вас не совпадают пароли!!!")
+      }
     }
-    const user = await instance.post('/auth/login', userData)
-    console.log(user.data)
-  }
+  };
 
   return (
-
     <div className={cl.routeAuth}>
-      <div className='container'>
+      <div className="container">
         <form className={cl.form} onSubmit={handleSubmit}>
           <Box
-            display='flex'
-            justifyContent='center'
-            alignItems='center'
-            flexDirection='column'
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
             maxWidth={640}
-            margin='auto'
+            margin="auto"
             padding={5}
-
             borderRadius={5}
-            boxShadow={'5px 5px 10px rgba(157, 213, 88, 0.75)'}
+            boxShadow={"5px 5px 10px rgba(157, 213, 88, 0.75)"}
           >
-            {location.pathname === '/Login' ? <Login setEmail={setEmail} setPassword={setPassword} /> : location.pathname === '/Register' ? <Register /> : null}
+            {location.pathname === "/Login" ? (
+              <Login setEmail={setEmail} setPassword={setPassword} />
+            ) : location.pathname === "/Register" ? (
+              <Register
+                setEmail={setEmail}
+                setPassword={setPassword}
+                setRepeatPassword={setRepeatPassword}
+                setFirstName={setFirstName}
+                setUsername={setUsername}
+              />
+            ) : null}
           </Box>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RouteAuth
+export default RouteAuth;
